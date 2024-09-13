1c1d1855653d82fa42f26bbe50385def2ee3fe7e
avanysouza <avanysouza@gmail.com>
2024-09-13 09:17:31 +0100

    update

--- FichaPratica1/Ex6.py
+++ FichaPratica1/Ex6.py
@@ -5,8 +5,9 @@
 valor1 = int(input('Digite o valor 1: '))
 valor2 = int(input('Digite o valor 2: '))
 
-valor2 = valor1
-valor1 = valor2
+valor1 = valor1 + valor2
+valor2 = valor1 - valor2
+valor1 = valor1 - valor2
 
 print(f"Valor 1 = {valor1}")
 print(f"Valor 2 = {valor2}")
