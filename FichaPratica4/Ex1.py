0f4c4edb21ba75698bbf24195a3282f4d8af3640
avanysouza <avanysouza@gmail.com>
2024-09-26 23:16:45 +0100

    ficha 4

--- FichaPratica4/Ex1.py
+++ FichaPratica4/Ex1.py
@@ -3,3 +3,33 @@
 # final, deverá perguntar ao utilizador se deseja repetir, permitindo-lhe efetuar novos cálculos.
 # Exemplo: Deseja continuar? (introduza s/n)
 
+def calculadora(num1, num2, operacao):
+    match operacao:
+        case '+':
+            return num1 + num2
+        case '-':
+            return num1 - num2
+        case '*':
+            return num1 * num2
+        case '/':
+            return num1 / num2
+        case _:
+            return "Operação inválida!"
+
+
+def main():
+    while True:
+        num1 = float(input("Introduza o primeiro valor: "))
+        num2 = float(input("Introduza o segundo valor: "))
+
+        operacao = input("Introduza a operação (+, -, *, /): ")
+
+        resultado = calculadora(num1, num2, operacao)
+        print(f"Resultado: {resultado}")
+
+        opcao = input("Deseja continuar? (introduza s/n): ")
+        if opcao != 's':
+            print("Encerrando o programa.")
+            break
+
+main()
